afe609af483671b672dff8684b8c3cb863e2ab0e
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-11-08 16:43:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.use(express.json())
 // app.use(cors())
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://movie-recommendation.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
