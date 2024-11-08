40125fc9828d1394b166101ecc96ae3b08644943
SHUBHAM PAL <140602488+ShubhamPal23@users.noreply.github.com>
2024-11-08 20:27:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://movie-recommendation-theta-hazel.vercel.app",
+    origin: "*",
     methods: ["POST", "GET", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
